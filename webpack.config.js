c6eeacf9a95e48ee6cc33ced884191a90826474b
Николай Гребенюк <Yanukhous@ukr.net>
2021-07-20 22:11:24 +0300

    добавил настройки

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'my-bundle.js',
-      },
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [],
+  },
+  devServer: {
+    open: true,
+    port: 9000,
+  },
 };
